6be3121a6ababba7dc0b588899a8cf6005f3e0a6
Max Kirchberger <max.kirchberger@siemens.com>
2023-12-02 13:16:09 +0100

    use invocation

--- scripts/buildscript.ps1
+++ scripts/buildscript.ps1
@@ -1,3 +1,4 @@
-$currentFolder = (Get-Location).Path
+$currentFolder = $MyInvocation.MyCommand.Path | Split-Path -Parent;
+#$currentFolder = (Get-Location).Path
 echo $currentFolder
 dotnet build ../src/Webserver.API/Webserver.API.csproj

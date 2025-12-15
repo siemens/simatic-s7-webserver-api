c51c3424bdce3cac3479b351220e4efa028fee10
Max Kirchberger <max.kirchberger@siemens.com>
2025-12-15 11:23:19 +0100

    use select to filter before access in loops

--- src/Webserver.API/Services/PlcProgram/ApiPlcProgramHandler.cs
+++ src/Webserver.API/Services/PlcProgram/ApiPlcProgramHandler.cs
@@ -224,9 +224,8 @@ namespace Siemens.Simatic.S7.Webserver.API.Services.PlcProgram
                             if (item.ArrayElements.Any())
                             {
                                 var requests = new List<IApiRequest>();
-                                foreach (var element in item.ArrayElements)
+                                foreach (var varNameForMethods in item.ArrayElements.Select(el => el.GetVarNameForMethods()))
                                 {
-                                    var varNameForMethods = element.GetVarNameForMethods();
                                     var requestToAdd = _requestFactory.GetApiPlcProgramBrowseRequest(ApiPlcProgramBrowseMode.Children, varNameForMethods);
                                     _logger?.LogTrace($"Add PlcProgram Browse request for '{varNameForMethods}' -> mode '{ApiPlcProgramBrowseMode.Children}'");
                                     requests.Add(requestToAdd);
